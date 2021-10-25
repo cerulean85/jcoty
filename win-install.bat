9806f5a73a3e38e339ece77568bc65021c6f7c45
cerulean85 <zhwan85@gmail.com>
2021-10-25 14:35:02 +0900

    Update Install File
    Update Install File

--- win-install.bat
+++ win-install.bat
@@ -9,11 +9,11 @@ SET SERVER_PATH=%PROJECT_PATH%\server
 CD %CLIENT_PATH%
 @START npm install
 
-SETX python38 "%LOCALAPPDATA%\Programs\Python\Python38" -m
-SETX PATH "%python38%;%PATH%" -m
+SETX PYTHON38 "%LOCALAPPDATA%\Programs\Python\Python38" /M
+SETX PATH "%python38%;%PATH%" /M
 
-SETX python38_scripts "%LOCALAPPDATA%\Programs\Python\Python38\Scripts" -m
-SETX PATH "%python38_scripts%;%PATH%" -m
+SETX PYTHON38_SCRIPTS "%LOCALAPPDATA%\Programs\Python\Python38\Scripts" /M
+SETX PATH "%python38_scripts%;%PATH%" /M
 
 pip install -r %SERVER_PATH%\requirements.txt
 
