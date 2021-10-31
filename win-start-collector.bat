0b5631dfbe818f992d5c397779cfb896871d664d
cerulean85 <zhwan85@gmail.com>
2021-10-31 23:53:58 +0900

    bat Update
    bat Update

--- win-start-collector.bat
+++ win-start-collector.bat
@@ -4,11 +4,11 @@ SETLOCAL
 
 SET SERVER_PATH=%cd%\server
 
-@START /B CMD /C python %SERVER_PATH%\url_collector_daemon.py
+@START /B CMD /C python %SERVER_PATH%\collector_daemon.py
 ECHO URL COLLECTOR started.
 
-@START /B CMD /C python %SERVER_PATH%\text_extractor.py
-ECHO TEXT EXTRACTOR started.
+::@START /B CMD /C python %SERVER_PATH%\text_extractor.py
+::ECHO TEXT EXTRACTOR started.
 
 @START /B CMD /C python %SERVER_PATH%\monitor.py
 ECHO MONITOR started.
